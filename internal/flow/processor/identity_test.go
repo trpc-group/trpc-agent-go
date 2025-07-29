51e8dce350c07be70cfbe035ad51fc689dae410d
wineandchord <guoqizhou123123@qq.com>
2025-07-29 10:50:05 +0800

    update

--- internal/flow/processor/identity_test.go
+++ internal/flow/processor/identity_test.go
@@ -89,7 +89,7 @@ func TestIdentityProc_Request(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			processor := NewIdentityRequestProcessor(
-				tt.agentName, 
+				tt.agentName,
 				tt.description,
 				WithAddNameToInstruction(true),
 			)
