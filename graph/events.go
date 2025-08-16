2f914db6954db5edf9616f101be5db9d866e12af
wineandchord <guoqizhou19@gmail.com>
2025-08-16 09:32:11 +0800

    change license

--- graph/events.go
+++ graph/events.go
@@ -1,12 +1,10 @@
 //
 // Tencent is pleased to support the open source community by making trpc-agent-go available.
 //
-// Copyright (C) 2025 Tencent.
-// All rights reserved.
+// Copyright (C) 2025 Tencent.  All rights reserved.
+
+// trpc-agent-go is licensed under the Apache License Version 2.0.
 //
-// If you have downloaded a copy of the tRPC source code from Tencent,
-// please note that tRPC source code is licensed under the  Apache 2.0 License,
-// A copy of the Apache 2.0 License is included in this file.
 //
 
 // Package graph provides graph-based workflow execution.
