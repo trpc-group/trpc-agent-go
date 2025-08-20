a07fb46b9af4f483c6ef50e9cb686dfa3bb53e2e
Rememorio <junevanlong@foxmail.com>
2025-08-20 20:20:10 +0800

    feat: add license

--- storage/tcvector/tcvector_test.go
+++ storage/tcvector/tcvector_test.go
@@ -1,3 +1,13 @@
+//
+// Tencent is pleased to support the open source community by making trpc-agent-go available.
+//
+// Copyright (C) 2025 Tencent.  All rights reserved.
+
+// trpc-agent-go is licensed under the Apache License Version 2.0.
+//
+//
+
+// Package tcvector provides the tcvectordb instance info management.
 package tcvector
 
 import (
