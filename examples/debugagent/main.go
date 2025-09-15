713cc2554b7a7ba64e0c1b5bde37cbba4f682ff5
Flash-LHR <2479082762@qq.com>
2025-09-15 17:53:06 +0800

    copyright

--- examples/debugagent/main.go
+++ examples/debugagent/main.go
@@ -1,8 +1,10 @@
+//
 // Tencent is pleased to support the open source community by making trpc-agent-go available.
 //
 // Copyright (C) 2025 Tencent.  All rights reserved.
 //
 // trpc-agent-go is licensed under the Apache License Version 2.0.
+//
 //
 
 // Package main demonstrates interactive chat using file operation tools.
