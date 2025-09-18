67cb45b5c6b33d37a4ef99710335ef8f5f7cfbd4
nomand-zc <wenzi1625@foxmail.com>
2025-09-19 00:39:11 +0800

    update

--- agent/invocation.go
+++ agent/invocation.go
@@ -300,8 +300,9 @@ func (inv *Invocation) AddNoticeChannelAndWait(ctx context.Context, key string, 
 }
 
 func panic_recover() {
+	nilPointerErr := "runtime error: invalid memory address or nil pointer dereference"
 	if r := recover(); r != nil {
-		if err, ok := r.(error); ok && err.Error() == "runtime error: invalid memory address or nil pointer dereference" {
+		if err, ok := r.(error); ok && err.Error() == nilPointerErr {
 			log.Error("noticeMu is uninitialized, please use agent.NewInvocaiton or Clone method to create Invocation.")
 			return
 		}
