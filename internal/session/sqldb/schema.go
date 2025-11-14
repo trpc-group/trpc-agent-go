d61355bce12f4e69a94f1b46f826337fcf0ebe3a
homerpan <homerpan@tencent.com>
2025-11-14 11:30:49 +0800

    add license on internal/session/sqldb

--- internal/session/sqldb/schema.go
+++ internal/session/sqldb/schema.go
@@ -1,3 +1,12 @@
+//
+// Tencent is pleased to support the open source community by making trpc-agent-go available.
+//
+// Copyright (C) 2025 Tencent.  All rights reserved.
+//
+// trpc-agent-go is licensed under the Apache License Version 2.0.
+//
+//
+
 // Package sqldb provides common utilities for SQL database-based session implementations.
 package sqldb
 
