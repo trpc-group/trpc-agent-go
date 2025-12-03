105bd14973ac77796b426ead1522b7b85c34ead7
wineandchord <guoqizhou19@gmail.com>
2025-12-03 10:21:11 +0800

    log: add context logger

--- log/log_test.go
+++ log/log_test.go
@@ -10,6 +10,7 @@
 package log_test
 
 import (
+	"context"
 	"testing"
 
 	"trpc.group/trpc-go/trpc-agent-go/log"
@@ -29,6 +30,24 @@ func TestLog(t *testing.T) {
 	log.Fatalf("test")
 }
 
+func TestContextHelpersUseContextDefault(t *testing.T) {
+	ctx := context.Background()
+
+	original := log.ContextDefault
+	defer func() {
+		log.ContextDefault = original
+	}()
+
+	logger := &countLogger{}
+	log.ContextDefault = logger
+
+	log.InfoContext(ctx, "test")
+
+	if logger.infoCalls != 1 {
+		t.Fatalf("expected infoCalls=1, got %d", logger.infoCalls)
+	}
+}
+
 type noopLogger struct{}
 
 func (*noopLogger) Debug(args ...any)                 {}
@@ -41,3 +60,23 @@ func (*noopLogger) Error(args ...any)                 {}
 func (*noopLogger) Errorf(format string, args ...any) {}
 func (*noopLogger) Fatal(args ...any)                 {}
 func (*noopLogger) Fatalf(format string, args ...any) {}
+
+type countLogger struct {
+	infoCalls int
+}
+
+func (*countLogger) Debug(args ...any)                 {}
+func (*countLogger) Debugf(format string, args ...any) {}
+func (c *countLogger) Info(args ...any) {
+	if len(args) == 0 {
+		return
+	}
+	c.infoCalls++
+}
+func (*countLogger) Infof(format string, args ...any)  {}
+func (*countLogger) Warn(args ...any)                  {}
+func (*countLogger) Warnf(format string, args ...any)  {}
+func (*countLogger) Error(args ...any)                 {}
+func (*countLogger) Errorf(format string, args ...any) {}
+func (*countLogger) Fatal(args ...any)                 {}
+func (*countLogger) Fatalf(format string, args ...any) {}
