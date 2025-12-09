cad1fd2bddff2c7f243db312c4c207f6b2869d2d
homerpan <homerpan@tencent.com>
2025-12-09 15:05:30 +0800

    optimize code

--- knowledge/vectorstore/tcvector/condition_converter.go
+++ knowledge/vectorstore/tcvector/condition_converter.go
@@ -14,6 +14,7 @@ import (
 	"fmt"
 	"reflect"
 	"runtime/debug"
+	"strings"
 
 	"github.com/tencent/vectordatabase-sdk-go/tcvectordb"
 	"trpc.group/trpc-go/trpc-agent-go/knowledge/searchfilter"
@@ -101,12 +102,7 @@ func (c *tcVectorConverter) buildLogicalCondition(cond *searchfilter.UniversalFi
 			condStr = childCond
 			continue
 		}
-
-		op := "and"
-		if cond.Operator == searchfilter.OperatorOr {
-			op = "or"
-		}
-		condStr = fmt.Sprintf("(%s) %s (%s)", condStr, op, childCond)
+		condStr = fmt.Sprintf("(%s) %s (%s)", condStr, strings.ToLower(cond.Operator), childCond)
 	}
 
 	if condStr == "" {
