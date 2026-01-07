ce7a504c8270da9cbc1efed8d55c887b774c340d
lincao <carolin913@126.com>
2026-01-07 23:03:47 +0800

    examples: add agui tdesign-chat demo (#772)
    为trpc-agent-go的agui协议提供tdesign chat版本的集成案例

---
+++ examples/agui/client/tdesign-chat/src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'tdesign-react/es/style/index.css';
+import '@tdesign-react/chat/es/style/index.js';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
