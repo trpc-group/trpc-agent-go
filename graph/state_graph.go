60f0b39eceb9f7e22c0ca60c720b3970e71b71a8
goodliu <amdahliu@tencent.com>
2026-01-08 18:45:08 +0800

    graph: introduce NodeFuncInterceptor for enhanced node function execution and telemetry (#1019)

--- graph/state_graph.go
+++ graph/state_graph.go
@@ -388,20 +388,30 @@ func WithModelCallbacks(callbacks *model.Callbacks) Option {
 // The name and description of the node can be set with the options.
 // This automatically sets up Pregel-style channel configuration.
 func (sg *StateGraph) AddNode(id string, function NodeFunc, opts ...Option) *StateGraph {
-	f := func(ctx context.Context, state State) (any, error) {
-		ctx, span := trace.Tracer.Start(ctx, itelemetry.NewWorkflowSpanName(fmt.Sprintf("execute_function_node %s", id)))
-		itelemetry.TraceWorkflow(span, &itelemetry.Workflow{Name: fmt.Sprintf("execute_function_node %s", id), ID: id})
-		defer span.End()
-		return function(ctx, state)
-	}
 	node := &Node{
-		ID:       id,
-		Name:     id,
-		Function: f,
-		Type:     NodeTypeFunction, // Default to function type
+		ID:   id,
+		Name: id,
+		Type: NodeTypeFunction, // Default to function type
 	}
 	for _, opt := range opts {
 		opt(node)
+	}
+
+	node.Function = func(ctx context.Context, state State) (any, error) {
+		ctx, span := trace.Tracer.Start(ctx, itelemetry.NewWorkflowSpanName(fmt.Sprintf("execute_function_node %s", id)))
+		workflow := &itelemetry.Workflow{Name: fmt.Sprintf("execute_function_node %s", id), ID: id, Request: state.safeClone()}
+		defer func() {
+			itelemetry.TraceWorkflow(span, workflow)
+			span.End()
+		}()
+
+		response, err := function(ctx, state)
+		if err != nil {
+			workflow.Error = err
+			return nil, err
+		}
+		workflow.Response = response
+		return response, nil
 	}
 
 	sg.graph.addNode(node)
@@ -1424,12 +1434,16 @@ func NewToolsNodeFunc(tools map[string]tool.Tool, opts ...Option) NodeFunc {
 
 	return func(ctx context.Context, state State) (any, error) {
 		ctx, span := trace.Tracer.Start(ctx, itelemetry.NewWorkflowSpanName("execute_tools_node"))
-		itelemetry.TraceWorkflow(span, &itelemetry.Workflow{Name: "execute_tools_node", ID: "execute_tools_node"})
-		defer span.End()
+		workflow := &itelemetry.Workflow{Name: "execute_tools_node", ID: "execute_tools_node", Request: state.safeClone()}
+		defer func() {
+			itelemetry.TraceWorkflow(span, workflow)
+			span.End()
+		}()
 
 		// Extract and validate messages from state.
 		toolCalls, err := extractToolCallsFromState(state, span)
 		if err != nil {
+			workflow.Error = err
 			return nil, err
 		}
 
@@ -1456,6 +1470,7 @@ func NewToolsNodeFunc(tools map[string]tool.Tool, opts ...Option) NodeFunc {
 			EnableParallel: parallel,
 		})
 		if err != nil {
+			workflow.Error = err
 			return nil, err
 		}
 		upd := State{StateKeyMessages: newMessages}
@@ -1488,6 +1503,7 @@ func NewToolsNodeFunc(tools map[string]tool.Tool, opts ...Option) NodeFunc {
 			}
 		}
 
+		workflow.Response = upd
 		return upd, nil
 	}
 }
