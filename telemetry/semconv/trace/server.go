59628336f78301558f84b23f69bd6d200b8f8c25
goodliu <amdahliu@tencent.com>
2026-01-13 11:12:13 +0800

    telemetry: enhance attribute report  for embedding (#1034)
    add new attribute:
    
    -   "gen_ai.embeddings.dimension.count"
    -  "server.address"
    - "server.port"

---
+++ telemetry/semconv/trace/server.go
@@ -0,0 +1,17 @@
+//
+// Tencent is pleased to support the open source community by making trpc-agent-go available.
+//
+// Copyright (C) 2025 Tencent.  All rights reserved.
+//
+// trpc-agent-go is licensed under the Apache License Version 2.0.
+//
+//
+
+package trace
+
+const (
+	// KeyServerAddress is the attribute key for server address.
+	KeyServerAddress = "server.address"
+	// KeyServerPort is the attribute key for server port.
+	KeyServerPort = "server.port"
+)
