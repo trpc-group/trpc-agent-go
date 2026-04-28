1eacfebb4b1e0bfe5cb81617a61d3dcd64aedd22
joey <zchengjoey@gmail.com>
2026-04-28 20:46:47 +0800

    feat: add e2b sandbox codeexecutor
    1. support codeexecutor WorkspaceManager
    2. support codeexecutor ProgramRunner
    
    Signed-off-by: joey <zchengjoey@gmail.com>

---
+++ codeexecutor/e2b/internal/codeinterpreter/example/main.go
@@ -0,0 +1,70 @@
+//
+// Tencent is pleased to support the open source community by making trpc-agent-go available.
+//
+// Copyright (C) 2025 Tencent.  All rights reserved.
+//
+// trpc-agent-go is licensed under the Apache License Version 2.0.
+//
+//
+
+// Example command demonstrating typical use of the Go Code Interpreter SDK.
+//
+// Usage:
+//
+//	E2B_API_KEY=e2b_... go run ./go/example
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"trpc.group/trpc-go/trpc-agent-go/codeexecutor/e2b/internal/codeinterpreter"
+)
+
+var (
+	code = `
+import numpy
+from PIL import Image
+
+imarray = numpy.random.rand(16,16,3) * 255
+image = Image.fromarray(imarray.astype('uint8')).convert('RGBA')
+
+image.save("test.png")
+print("Image saved.")`
+)
+
+func main() {
+	ctx := context.Background()
+
+	sbx, err := codeinterpreter.Create(ctx, &codeinterpreter.SandboxOpts{
+		Timeout: 60 * time.Second,
+	})
+	if err != nil {
+		log.Fatalf("create sandbox: %v", err)
+	}
+
+	fmt.Println("ℹ️  sandbox created", sbx.SandboxID())
+	defer func() {
+		_ = sbx.Kill(ctx)
+		fmt.Println("🧹 sandbox killed")
+	}()
+
+	exec, err := sbx.RunCode(ctx, code, &codeinterpreter.RunCodeOpts{
+		OnStdout: func(msg codeinterpreter.OutputMessage) {
+			fmt.Printf("[stdout] %s", msg.Line)
+		},
+		OnStderr: func(msg codeinterpreter.OutputMessage) {
+			fmt.Printf("[stderr] %s", msg.Line)
+		},
+	})
+	if err != nil {
+		log.Fatalf("run code: %v", err)
+	}
+
+	fmt.Println("result:", exec.Results[0].PNG)
+	if exec.Error != nil {
+		fmt.Println("error:", exec.Error)
+	}
+}
