c0ca4ac8e7c8da57f7aec7b99d55677fe4710c0a
Nene7ko_ <1604009816@qq.com>
2026-07-26 19:52:48 +0800

    docs(a2a): document anonymous client cookie continuity

--- examples/a2amultipath/client/main.go
+++ examples/a2amultipath/client/main.go
@@ -14,6 +14,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
+	"net/http/cookiejar"
 
 	"trpc.group/trpc-go/trpc-a2a-go/client"
 	"trpc.group/trpc-go/trpc-a2a-go/protocol"
@@ -29,7 +31,14 @@ func main() {
 	message := flag.String("msg", defaultMessage, "User message")
 	flag.Parse()
 
-	c, err := client.NewA2AClient(*agentURL)
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatalf("create cookie jar: %v", err)
+	}
+	c, err := client.NewA2AClient(
+		*agentURL,
+		client.WithHTTPClient(&http.Client{Jar: jar}),
+	)
 	if err != nil {
 		log.Fatalf("create client: %v", err)
 	}
